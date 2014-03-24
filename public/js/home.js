72a1b491b6a5edfc3987131211bcec786c468959
ryansukale <ryansukale@gmail.com>
2014-03-24 14:09:04 -0400

    Select project and go to newTopic creation page

--- public/js/home.js
+++ public/js/home.js
@@ -8,22 +8,33 @@ $(function(){
 	
 	var tmpl = {
 		repoLI : _.template($('#_tmplRepoLI').html()),
-		convoLI : _.template($('#_tmplConvoLI').html())
+		convoLI : _.template($('#_tmplConvoLI').html()),
+		projectSelectorLI : _.template($('#_tmplProjectSelectorLI').html())
 	};
 	
+	var selectedProjectId='';
+		
 	function init(){
 	
 		$.ajax(urls.allRepos)
 			.done(function( data, textStatus, jqXHR ) {
 				//console.log(data);
 				var htmlElems = [];
+				var modalhtmlElems = [];
 				_.each(data, function(element, index, list){
 					//console.log(tmpl.repoLI(element));
 					htmlElems.push(tmpl.repoLI(element));
+					modalhtmlElems.push(tmpl.projectSelectorLI(element));
 				});
 				
 				//console.log(htmlElems.join(''));
 				$('.repo-list ul').append(htmlElems.join(''));
+				
+				$('.project-list-selection').append(modalhtmlElems.join(''));
+				
+				_.each($('.project-list-selection').children(),function(element, index, list){
+					bindProjectSelection(element);
+				});
 				
 			});
 			
@@ -43,7 +54,26 @@ $(function(){
 
 	}
 	
+	function bindProjectSelection(selector){
+		var $element = $(selector);
+		$element.on('click',function(e){
+			var projectId = $element.data('projectid');
+			window.location = 'newTopic.html?projectId='+projectId;
+		})
+	}
+	
 	function bindEventHandlers(){
+		
+		$('.new-topic').on('click',function(e){
+			
+			if(selectedProjectId){
+				window.location = 'newTopic.html?projectId='+selectedProjectId;
+			}else{
+				$('.launch-project-selector').trigger('click');
+			}
+			
+		});
+		
 	};
 		
 	init();
---
+++ public/js/newTopic.js
@@ -0,0 +1,523 @@
+$(function(){
+	
+	var urls={
+		uploads:'./responses/upload',
+		topicsDetailsFor:'/topics',
+		recordings:'recordings',
+		userInfo:'/userinfo',
+		saveTags:'./responses/tags/',
+		saveTagsForResponse:'./responses/tags',
+		responsesFor:'/responses',
+		membersForProject:'/projects/members'
+	},
+	rtcOptions = {
+		'buffer-size': 16384,
+	};
+	
+	var itemId='',
+	recordingId='',
+	itemType='',
+	userInfo={},
+	pageData={};
+	
+	var countdownTime = 5000, // Max duration for audio recording
+	intervalTime = 1000, // Countdown for recording
+	intervalId  = '';
+	
+	//Fetch the userInfo
+	
+	$.ajax(urls.userInfo)
+		.done(function( data, textStatus, jqXHR ) {
+			userInfo=data;
+			pageData.userInfo=data;
+			//console.log('userInfo',userInfo);
+		});
+	
+	function updateUserInfo(){
+		$.ajax(urls.userInfo)
+			.done(function( data, textStatus, jqXHR ) {
+				pageData.userInfo=data;
+				console.log(pageData.userInfo);
+				$('.navbar .total-points').html(pageData.userInfo.points).addClass('hidden');
+				$('.navbar .updated-points').html(pageData.userInfo.points).removeClass('hidden');
+				
+				$('.navbar .updated-points').animate({
+					opacity:0
+				},
+				6000,
+				function(){
+					$(this).addClass('hidden').css({opacity:1});
+					$('.navbar .total-points').removeClass('hidden');
+				});
+				
+			});
+	}
+	
+	var tmpl = {
+		topic : _.template($('#_tmplTopic').html()),
+		textResponse : _.template($('#_tmplTextResponse').html()),
+		audioResponse : _.template($('#_tmplAudioResponse').html()),
+		recPanelDefault : _.template($('#_tmplRecPanel_Default').html()),
+		responseTags : _.template($('#_tmplResponseTags').html()),
+		memberLI : _.template($('#_tmplMemberLI').html())
+	}
+	
+	function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	}
+	
+	var topicId = getParameterByName('id');
+	
+	//If the user is selecting a particular topic, render the topic details interface
+	if(topicId){
+		
+		$.ajax([urls.topicsDetailsFor,topicId].join('/'))
+			.done(function( data, textStatus, jqXHR ) {
+				
+				//Save all the data that is used to render the contents of this page.
+				pageData.topicInfo = data;
+				
+				var itemInfo = {
+					itemId:pageData.topicInfo.id,
+					projectId:pageData.topicInfo.project_id,
+					itemType:'topic'
+				};
+				
+				$('.topic-details .topic')
+					.html(tmpl.topic(pageData.topicInfo));
+					
+				$('.topic-details')
+					.attr('data-itemInfo',JSON.stringify(itemInfo));
+					
+				bindRecordControls();
+				getProjectMembers();
+				
+			});
+	
+		$.ajax([urls.responsesFor,topicId].join('/'))
+			.done(function( data, textStatus, jqXHR ) {
+				
+				//Save all the data that is used to render the contents of this page.
+				pageData.responses = data.responses;
+				
+				var responsesArray = [];
+				
+				_.each(pageData.responses, function(responseDtls, index, list){
+					
+					if(responseDtls.type==="text"){
+						responsesArray.push(tmpl.textResponse(responseDtls));
+					}else{
+						if(responseDtls.type==="audio"){
+							responsesArray.push(tmpl.audioResponse(responseDtls));
+						}
+					}
+					
+				});
+				
+				$otherResponsesContainer = $('.response-details .other-responses');
+				
+				$otherResponsesContainer.append(responsesArray.join(""));
+				
+				_.each($otherResponsesContainer.children(), function(responseItem, index, list){
+					bindResponseHandlers(responseItem);
+				});
+				
+				updateTopicMessages();
+				//bindHandlers();
+				
+			});
+	
+		function getProjectMembers(){
+			
+			var projectId = pageData.topicInfo.project_id;
+			
+			$.ajax([urls.membersForProject,projectId].join('/'))
+				.done(function( data, textStatus, jqXHR ) {
+					pageData.projectMembers=data.members;
+					
+					var memberItems = [];
+					_.each(pageData.projectMembers, function(memberDetails, index, list){
+						memberItems.push(tmpl.memberLI(memberDetails));
+					});
+					
+					$('.member-list').append(memberItems.join(''));
+					
+					pageData.mentionsData = [];
+					_.each(pageData.projectMembers, function(member, index, list){
+						pageData.mentionsData.push(member.username);
+					});
+					
+					$('textarea[name="responseTags"]').triggeredAutocomplete({
+						hidden: '#hidden_inputbox',
+						source: pageData.mentionsData,
+							 trigger: "@" ,
+							 allowDuplicates: false
+						});
+						
+				});
+		};
+		
+	}
+	
+	function bindResponseHandlers(selector){
+		var $responseItem = $(selector);
+		//console.log('here');
+		$responseItem.find('.add-tags a').on('click',function(e){
+			
+			var responseId = $responseItem.find('[name="responseId"]').val(),
+				currentResponse = _.findWhere(pageData.responses, {"id": responseId});
+		
+			$responseItem.find('.prompts').addClass('hidden')
+				.children('.add-tags').addClass('hidden');
+			
+			//Get the current tags for the response
+			var currentTagsArr = [];
+			_.each(currentResponse.tags,function(element, index, value){
+				currentTagsArr.push(element);
+			});
+			
+			$responseItem.find('textarea[name="responseTags"]')
+					.val(currentTagsArr.join(' '))
+					.removeClass('hidden');
+			
+			$responseItem.find('.tag-update-actions')
+				.removeClass('hidden');
+				
+			$responseItem.find('.tag-list').addClass('hidden');
+			
+			e.preventDefault();
+		});
+		
+		$responseItem.find('.tag-update-actions').on('click',function(e){
+			var $this = $(this),
+				$target = $(e.target);
+			
+			var responseId = $responseItem.find('[name="responseId"]').val();
+			
+			if($target.hasClass('cancel')){
+			
+				//Fetch the existing values
+				var currentResponse = _.findWhere(pageData.responses, {"id": responseId});
+				
+				//Show/hide the appropriate elements
+				if(currentResponse.tags.length===0){
+				
+					//Show the prompts to add tags
+					$responseItem.find('.prompts').removeClass('hidden')
+					.children('.add-tags').removeClass('hidden');
+					
+				}else{
+					var tagListItems = tmpl.responseTags({tags:currentResponse.tags});
+					$responseItem.find('.tag-list').html(tagListItems)
+						.removeClass('hidden');
+				}
+				
+				//Clear the textarea				
+				$responseItem.find('textarea[name="responseTags"]')
+					.val('')
+					.addClass('hidden');
+				
+				//Hide the tag update actions
+				$responseItem.find('.tag-update-actions')
+					.addClass('hidden');
+				
+			}else{
+				if($target.hasClass('save')){
+					var rawTagString = $responseItem.find('textarea[name="responseTags"]').val();
+					
+					var correctedTagsArr = getPrefixedTagArr(rawTagString);
+					
+					console.log(correctedTagsArr);
+					
+					$.ajax({
+						url:[urls.saveTagsForResponse,responseId].join('/'),
+						type:'POST',
+						data:{
+							tags:correctedTagsArr
+						}
+					})
+					.done(function(data){
+					
+						var currentResponse = _.findWhere(pageData.responses, {"id": responseId});
+						currentResponse.tags = data.tags;
+						
+						var tagListItems = tmpl.responseTags({tags:currentResponse.tags});
+						$responseItem.find('.tag-list').html(tagListItems)
+							.removeClass('hidden');
+						
+						//Clear the textarea				
+						$responseItem.find('textarea[name="responseTags"]')
+							.val('')
+							.addClass('hidden');
+						
+						//Hide the tag update actions
+						$responseItem.find('.tag-update-actions')
+							.addClass('hidden');
+							
+					});
+					
+				}
+			}
+			
+		});
+		
+		$responseItem.find('.edit').on('click',function(e){
+			$responseItem.find('.add-tags a').trigger('click');
+		});
+		
+		//console.log(pageData.mentionsData);
+		if(pageData.mentionsData){
+			$responseItem.find('textarea[name="responseTags"]').triggeredAutocomplete({
+				hidden: '#hidden_inputbox',
+				source: pageData.mentionsData,
+					 trigger: "@" ,
+					 allowDuplicates: false
+				});
+		}
+		
+	}
+	
+	function bindActionable(selector){
+		
+		$(selector).on('click',function(event){
+			var $this = $(this);
+			var $target = $(event.target);
+			
+			var $parentRecPanel = $target.parents('.rec-panel');
+			
+			//Check if the action is from the recording panel
+			if($parentRecPanel.length>0){
+				
+				if($target.hasClass('submit')){
+					//console.log('submitting!');
+					
+					var itemInfo = JSON.parse($target.parents('.actionable').attr('data-itemInfo'));
+					
+					var $parentRecPanel = $target.parents('.rec-panel');
+					
+					var rawTagString = $parentRecPanel.find('textarea[name="recTags"]').val();
+					
+					var rawTextArr = _.without(rawTagString.split(' '),''); //get all the tags and clear the spaces
+
+					var correctedTagsArr = _.map(rawTextArr, function(tagText){
+					
+							var finalText;
+							var firstChar = tagText.charAt(0);
+							if(firstChar === '@' || firstChar === '#'){
+								finalText=tagText;
+							}else{
+								finalText='#'+tagText;
+							}
+							
+							return finalText; 
+					});
+					
+					console.log(pageData.responses);
+					
+					var responseId = $parentRecPanel.find('.response-id').val();
+					
+					console.log('tags',correctedTagsArr);
+					$.ajax({
+						url:urls.saveTags+responseId,
+						type:'POST',
+						data:{
+							tags:correctedTagsArr
+						}
+					})
+					.done(function(data){
+						console.log(data);
+					});
+					
+					event.preventDefault();
+					
+				}
+				
+			}
+			
+		});
+		
+	}
+	
+	function getPrefixedTagArr(rawTagString){
+	
+		var rawTextArr = _.without(rawTagString.split(' '),''); //get all the tags and clear the spaces
+		var correctedTagsArr = _.map(rawTextArr, function(tagText){
+					
+				var finalText;
+				var firstChar = tagText.charAt(0);
+				if(firstChar === '@' || firstChar === '#'){
+					finalText=tagText;
+				}else{
+					finalText='#'+tagText;
+				}
+				
+				return finalText;
+		});
+		
+		return correctedTagsArr;
+	}
+	
+	function bindRecordControls(){
+	
+		bindActionable('.actionable');
+	
+		$('.record-prompt').on('click',function(event){
+			var $this = $(this);
+			var $target = $(event.target);
+			
+			$this.parents('.actionable').find('.rec-panel').removeClass('hidden');
+			
+		});
+		
+		
+		$('.rec-control').on('click',function(event){
+			var $this = $(this);
+			var $target = $(event.target);
+			
+			var $parentRecPanel = $this.parents('.rec-panel');
+			
+			if($this.hasClass('start-rec')){
+			
+				navigator.getUserMedia({audio: true}, function(mediaStream) {
+
+					window.recordRTC = RecordRTC(mediaStream,rtcOptions);
+					recordRTC.startRecording();
+					
+					$this.text('Stop');
+					$this.toggleClass('start-rec stop-rec');
+					
+					$parentRecPanel.find('.control-feedback .info.recording').removeClass('hidden');
+					
+					var $countdown = $parentRecPanel.find('.countdown');
+					$countdown.html(countdownTime/1000);
+
+					intervalId = setInterval(function(){
+						
+						
+						var countdownValue = +($countdown.html());
+						
+						countdownValue=countdownValue-1;
+						
+						$countdown.html(countdownValue);
+						
+						if(countdownValue==0){
+							$parentRecPanel.find('.stop-rec').click();
+							clearInterval(intervalId);
+						}
+						
+						
+					},intervalTime);
+
+				});
+				
+			}else{
+				
+				if($this.hasClass('stop-rec')){
+				
+					clearInterval(intervalId);
+					$parentRecPanel.find('.control-feedback .info.recording').addClass('hidden');
+					$parentRecPanel.find('.control-feedback .info.processing').removeClass('hidden');
+					
+					recordRTC.stopRecording(function(audioURL) {
+					//window.open(audioURL);
+					console.log(audioURL);
+					//recordRTC.save();
+					
+					var itemInfo = JSON.parse($this.parents('.actionable').attr('data-itemInfo'));
+					//console.log('itemInfo',itemInfo);
+					
+					var formData = new FormData();
+					formData.append('recording', recordRTC.getBlob());
+					formData.append('itemId', itemInfo.itemId);
+					formData.append('itemType', itemInfo.itemType);
+					formData.append('projectId', itemInfo.projectId);
+					
+						xhr(urls.uploads, formData, function (response) {
+								//console.log(response);
+								
+								response = JSON.parse(response);
+								//Add the response details to the existing responses to on the page
+								pageData.responses.push(response.responseDtls);
+								
+								//Render a new block of the response on the page //TODO
+								console.log(response.responseDtls);
+								$('.response-details .other-responses')
+									.prepend(tmpl.audioResponse(response.responseDtls));
+								var $newResponse = $('.response-details .other-responses').children().eq(0);
+								
+								bindResponseHandlers($newResponse);
+								
+								$newResponse.find('.notifications').removeClass('hidden')
+									.children('.new').removeClass('hidden');
+								
+								$this.text('Done');
+								
+								var $parentRecPanel = $this.parents('.rec-panel');
+								
+								$parentRecPanel.find('.response-id').val(response.responseDtls.id);
+								
+								$parentRecPanel.find('.rec-details').removeClass('hidden').find('.rec-link').attr('href',audioURL);
+								//$this.toggleClass('start-rec stop-rec');
+								$this.removeClass('start-rec stop-rec rec-control btn btn-primary');
+								$this.addClass('alert alert-info');
+								
+								$parentRecPanel.find('.final-actions').removeClass('hidden');
+								$parentRecPanel.find('.control-feedback .info.processing').addClass('hidden');
+								
+								//Transfer tags if any to the new response, and display appropriate response status
+								
+								//Reset the recording panel to its original state
+								resetRecPanel();
+								updateUserInfo();
+								updateTopicMessages();
+								//Toggle height/close the rec panel
+								
+						});
+						
+					});
+					
+				}
+				
+			}
+
+		});
+		
+	}
+	
+	function updateTopicMessages(){
+		if(pageData.responses.length>0){
+			$('.topic-details .messages .prompt-first').addClass('hidden');
+		}
+	}
+	
+	function resetRecPanel(){
+		var recPanelDefault = tmpl.recPanelDefault();
+		
+		$('.rec-panel').children().remove();
+		$('.rec-panel').append(recPanelDefault);
+		bindRecordControls();
+		
+	}
+	
+	function bindHandlers(){
+	
+		//bindRecordControls();
+		
+	}
+
+	
+	function xhr(url, data, callback) {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			if (request.readyState == 4 && request.status == 200) {
+				callback(request.responseText);
+			}
+		};
+		request.open('POST', url);
+		request.send(data);
+	}
+	
+});
