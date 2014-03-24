a298eda221ceec325b1ea1521b5dd87bb988a3e0
ryansukale <ryansukale@gmail.com>
2014-03-24 14:45:56 -0400

    Highlighting tags of the current user

--- public/js/home.js
+++ public/js/home.js
@@ -3,6 +3,7 @@ $(function(){
 	var urls = {
 		allRepos : '/projects',
 		parentRepos : '/getParentRepos',
+		userInfo : '/userinfo',
 		topics : '/topics'
 	};
 	
@@ -11,6 +12,21 @@ $(function(){
 		convoLI : _.template($('#_tmplConvoLI').html()),
 		projectSelectorLI : _.template($('#_tmplProjectSelectorLI').html())
 	};
+	
+	pageData={};
+	
+	function getUserInfo(callback){
+		
+		$.ajax(urls.userInfo)
+		.done(function( data, textStatus, jqXHR ) {
+			userInfo=data;
+			pageData.userInfo=data;
+			if(callback) callback(pageData.userInfo);
+		});
+		
+	};
+	
+	getUserInfo();
 	
 	var selectedProjectId='';
 		
@@ -50,8 +66,23 @@ $(function(){
 				//console.log(htmlElems.join(''));
 				$('.convo-list ul').append(htmlElems.join(''));
 				
+				if(!pageData.userInfo){
+					getUserInfo(highlightMyTags);
+				}else{
+					highlightMyTags(pageData.userInfo);
+				}
+				
 			});
 
+	}
+	
+	function highlightMyTags(userInfo){
+		_.each($('.convo-details .tag'), function(element, index, list){
+			var $currentTag = $(element);
+			if($currentTag.html()==='@'+userInfo.login){
+				$currentTag.addClass('highlighted-tag');
+			}
+		});
 	}
 	
 	function bindProjectSelection(selector){
