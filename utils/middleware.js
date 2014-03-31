d17230babc201cd465e5f1c0e0ec4a21b0225efb
ryansukale <ryansukale@gmail.com>
2014-03-31 09:43:03 -0400

    Setting up user session in middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,5 @@
-var filename = __filename.slice(__filename.lastIndexOf(path.sep)+1);
+var filename = __filename.slice(__filename.lastIndexOf(path.sep)+1),
+fs = require('fs');
 
 module.exports = {
 	
@@ -8,6 +9,15 @@ module.exports = {
 			//The user session does not exist
 			console.log('User session does not exist');
 			
+			if(env.phase==='prototype'){
+				var userId = ''+env.DEMOUSERID || 100; //Default userId 100
+				var userInfoFilePath = [rootDir,'data',userId,'user.json'].join(path.sep);
+				var userInfo = JSON.parse(fs.readFileSync(userInfoFilePath));
+				req.session.userInfo = userInfo;
+			}else{
+				//Setup a 'guest' read only user
+			}
+			console.log("Current user:"+req.session.userInfo.login);
 		}
 		
 		next();
